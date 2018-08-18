540ba761921cbb063d7baa8c487d252604944135
simonalu <simonalu@kth.se>
2018-08-18 11:38:53 +0200

    Converter begun
    I have begun working on the converter csv -> rdf. It seems to work wonderfully.
    
    *It handles uid relations, langs and std values.
    *It also writes to a file
    
    #1

---
+++ tools/convert.ps1
@@ -0,0 +1,33 @@
+cls
+$input = "E:\User\Simon\document\GitHubE\olandr\detamata\tools\test.csv"
+$output = "E:\User\Simon\document\GitHubE\olandr\detamata\tools\test.rdf"
+rm $output -ea ig
+
+$header = (Get-Content $input | Select-Object -First 1).split(";")
+$csv = Import-csv $input -Delimiter ";"
+
+# Stores all the possible unique identifiers --- i.e. the first cell on each row.
+$uid = $csv."$($header[0])"
+
+$langs = "da","nl","en","fi","fr","de","hu","it","no","pt","ro","ru","es","sv","tr","zh","ja","ko"
+$L = 0
+
+for ($row = 0; $row -lt $uid.length; $row++){
+  # We reset all the lang if need be
+  $L = 0
+  for ($col = 1; $col -lt $header.length-1; $col++){
+
+    $uniqueID = $uid[$row]
+    $predicate = $header[$col]
+    $relation = $csv[$row]."$predicate"
+
+    # We need to differentiate whether the attribute is a uid-relation, multiple alternative (@lang) or just a value.
+    # This identifier is removed in the output.
+    switch ("$predicate"[0]) {
+      "!" {$relation = "_:$($relation)"}
+      "@" {if ($L+1 -gt $langs.length) {$predicate += "*ERROR ALL LANGS USED*"}; $relation = "`"$($relation)@$($langs[$L])`"";$L++}
+      default {$relation = "`"$($relation)`""}
+    }
+    ac $output "$uniqueID <$($predicate.Substring(1))> $relation ."
+  }
+}
